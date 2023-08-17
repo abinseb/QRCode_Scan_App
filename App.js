f5ec093989c737de164e7199ad549e6d3bfcaabb
abin sebastian <abinseb09@gmail.com>
2023-08-17 13:42:50 +0530

    firstcommit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Scan from './Pages/Common/Scan';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Scan'
+        screenOptions={{headerShown:false,}}
+      >
+        <Stack.Screen name='Scan' component={Scan} />
+
+      </Stack.Navigator>
+      
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Pages/Common/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View ,StyleSheet} from 'react-native'
+
+const Home=()=>{
+    return(
+        <View style={Styles.container}>
+
+
+        </View>
+    )
+}
+export default Home;
+
+const Styles = StyleSheet.create({
+    flex:1,
+
+})
